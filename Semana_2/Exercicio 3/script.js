e26e82cd3cf87b81aa896500be9c104452e9a107
Vitor Hugo Belorio Simão <vitorhugobsimao@gmail.com>
2025-02-18 14:31:43 -0300

    :recycle: refactor: Refazendo o exercício 3 para aceitar frases

--- Semana_2/Exercicio 3/script.js
+++ Semana_2/Exercicio 3/script.js
@@ -1,14 +1,19 @@
-document.getElementById("botao").addEventListener("click", 
-    () => {
-        const texto = document.getElementById('texto').value;
-        const textoLimpo = texto.replace(/[\W_]/g, '').toLowerCase();; // deixa tudo minusculo e remove caractéres especiais
-        const textoInvertido = textoLimpo.split('').reverse().join('');
-        
-        if (textoLimpo === textoInvertido) {
-            alert("É um palíndromo!");
-        } 
-        else {
-            alert("Não é um palíndromo.");
-        }
+document.getElementById("botao").addEventListener("click", () => {
+    const texto = document.getElementById('texto').value.toLowerCase();
+    const palavras = texto.toLowerCase().split(" "); // quebra a frase em palavras
+    
+    const palavrasInvertidas = palavras.map(palavra => 
+        palavra.replace(/[\W_]/g, '').split('').reverse().join('') // inverte cada palavra e junta depois
+    );
+
+    const textoInvertido = palavrasInvertidas.join(' ').toLowerCase();
+
+    //console.log(texto); 
+    //console.log(textoInvertido); 
+
+    if (texto.toLowerCase().replace(/[\W_]/g, '') === textoInvertido.replace(/[\W_]/g, '')) {
+        alert("É um palíndromo!");
+    } else {
+        alert("Não é um palíndromo.");
     }
-)
+});
