1c1662a19b335b62e7b82f92223fe0bec8c7ea59
VitorHugoBelorio <vitorhugobsimao@gmail.com>
2025-02-18 11:27:28 -0300

    :recycle: refactor: Reajustando o formato de exibição das atividades e removendo o JS nas linhas do HTML

---
+++ Semana_1/Exercício 1/script.js
@@ -0,0 +1,21 @@
+document.getElementById("botao").addEventListener("click", 
+    () => {
+        let numero = document.getElementById('numero').value;
+        if(!isNaN(numero) && numero >= 0)
+        {
+            if(numero % 2 == 0)
+            {
+                alert(`O número: ${numero} é par`);
+            }
+            else
+            {
+                alert(`O número: ${numero} é impar`);
+            }
+        }
+        else
+        {
+            alert("O conteúdo digitado não é um número ou não é positivo");
+        }
+    }
+);
+
---
+++ Semana_1/Exercício 4/script.js
@@ -0,0 +1,27 @@
+document.getElementById("botao").addEventListener("click", 
+    () => {
+        let dado = prompt("Digite um valor:");
+    
+        if (dado === null) {
+            document.body.innerHTML = "<p>Obrigado por visitar esta página.</p>";
+            return;
+        }
+    
+        let desejaVerificar = confirm("Você deseja verificar o tipo do dado informado?");
+        
+        if (desejaVerificar) {
+            let tipo = typeof dado;
+    
+            
+            if (!isNaN(dado) && dado.trim() !== "") {
+                tipo = "number";
+            }
+    
+            document.body.innerHTML = `<p>O tipo do dado informado é: <strong>${tipo}</strong></p>`;
+        } else {
+            document.body.innerHTML = "<p>Obrigado por visitar esta página.</p>";
+        }
+    }
+    
+);
+
