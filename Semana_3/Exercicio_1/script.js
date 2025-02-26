ae2c082f96b1f1336bd8b0a94ac60e655a5d3c57
VitorHugoBelorio <vitorhugobsimao@gmail.com>
2025-02-26 11:51:43 -0300

    :sparkles: feat: Resolução do exercício 1 da semana 3

---
+++ Semana_3/Exercicio_1/script.js
@@ -0,0 +1,21 @@
+var array = [];
+
+document.getElementById("botao").addEventListener("click", () => {
+    const conteudo = document.getElementById("conteudo").value;
+   
+    array.push(conteudo);
+
+    array.sort();
+
+    const lista = document.getElementById("lista"); 
+    
+    
+    lista.innerHTML = ""; // Limpa a lista exibida  
+
+    for (let i = 0; i < array.length; i++) {
+        var li = document.createElement("li"); 
+        var valorArray = document.createTextNode(`${array[i]}`);
+        li.appendChild(valorArray); 
+        lista.appendChild(li); 
+    }
+});
